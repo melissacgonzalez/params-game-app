931e18d0410b57d8e4b5760105f5b818a5a1097d
Melissa Gonzalez <marvelissa@gmail.com>
2017-07-06 19:34:48 -0500

    Add Guessing Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,12 @@
 class GamesController < ApplicationController
   def name_method
     @name = params["name"]
-    @caps_name = @name.upcase
     render "name_game.html.erb"
+  end
+
+  def number_method
+    @answer = 36
+    @guess = params["guess"].to_i
+    render "number_game.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/name_game" => "games#name_method"
+  get "/number_game" => "games#number_method"
 end
