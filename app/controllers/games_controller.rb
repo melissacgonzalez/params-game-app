d2e2b72be80ffae82c5116dd4fce3f74c2c3bd1a
Melissa Gonzalez <marvelissa@gmail.com>
2017-07-06 20:35:14 -0500

    Add Guessing Game using URL Segment Params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,16 +9,31 @@ class GamesController < ApplicationController
 
   def number_method
     answer = 36
-    guess = params["guess"].to_i
-    if answer == guess
+    @guess = params["guess"].to_i
+    if answer == @guess
       @message = "You won!"
-    elsif answer < guess
+    elsif answer < @guess
       @message = "Guess Lower"
-    elsif answer > guess
+    elsif answer > @guess
       @message = "Guess Higher"
     else
       @message = "Please enter a number."
     end
     render "number_game.html.erb"
+  end
+
+  def guess_url_segment_method
+    answer = 36
+    @guess = params["guess"].to_i
+    if answer == @guess
+      @message = "You won!"
+    elsif answer < @guess
+      @message = "Guess Lower"
+    elsif answer > @guess
+      @message = "Guess Higher"
+    else
+      @message = "Please enter a number."
+    end
+    render "guess_url_segment.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/name_game" => "games#name_method"
   get "/number_game" => "games#number_method"
+  get "/guess_url_segment/:guess" => "games#guess_url_segment_method"
 end
